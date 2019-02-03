8c21c2840a737ecce262cc616fe2a4c9d67f3e91
Jay Nielsen <jayarnielsen@gmail.com>
2019-02-03 10:56:12 -0700

    initia wp export

---
+++ app/themes/dototot/js/app.js
@@ -0,0 +1,23 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+
+document.querySelectorAll('pre').forEach((block) => {
+  hljs.highlightBlock(block);
+});
+
+jQuery(document).ready(function($){
+    $(document).foundation();
+
+    $('#home-carousel').slick({
+        lazyLoad: 'ondemand',
+        autoplay: true,
+        autoplaySpeed: 5500,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<i class="prev fa fa-angle-left"></i>',
+        nextArrow: '<i class="next fa fa-angle-right"></i>',
+        initialSlide: Math.floor(Math.random() * ($('#home-carousel img').length)),
+    });
+});
